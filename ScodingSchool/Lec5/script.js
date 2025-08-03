f883865be378b93bf1b56691217ce6772047e88b
Akash504-ai <santraakash999@gmail.com>
2025-08-03 20:26:23 +0530

    Lec5 commit

--- ScodingSchool/Lec5/script.js
+++ ScodingSchool/Lec5/script.js
@@ -2,3 +2,16 @@
 //handel backend process of forms and making sure that data coming from any frontend lib, fw, templating engins, we still handel it at the backend
 
 //session cookie
+
+//cookie ---> 
+//----A cookie is a small piece of data stored in your browser by a website.----//
+// What it stores:
+//Your name ,Login info,Preferences (like dark mode),Items in your shopping cart.
+
+//session ---> 
+//----A session is a way for the server to remember who you are between different requests.----//
+// What it stores:
+// Your login status
+// Your cart items
+// Your role (admin/user)
+// Anything private stored on the server
