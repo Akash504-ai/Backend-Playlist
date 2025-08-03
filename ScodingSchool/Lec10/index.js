cc36a608cefc6d950a509a9202db60dd4d5de410
Akash504-ai <santraakash999@gmail.com>
2025-08-03 23:54:43 +0530

    Lec10 commit

---
+++ ScodingSchool/Lec10/index.js
@@ -0,0 +1,16 @@
+//Data Storage
+
+// -- database can store anything
+
+//Type of DBS - SQL(data stored in tabel formet) | NoSQL(data stored in object form)
+
+// NoSQL - mongoDB
+
+//what and why
+//terminologies - collections, documents, schemas, keys, models
+
+// CODE                         DATABASE
+// -----------------------------------------------------
+// mongoose.connect    ->      database create
+// model create        ->      collection
+// CREATE code         ->      document
