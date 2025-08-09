2562979baf9f91822eef7e99b344c9a5a07f8d93
Akash504-ai <santraakash999@gmail.com>
2025-08-09 19:46:11 +0530

    server info commit

--- Hello_World_YT_Channel/Lec_on_db/server.js
+++ Hello_World_YT_Channel/Lec_on_db/server.js
@@ -5,6 +5,13 @@ const port = 3000;
 
 const bodyPerser = require("body-parser");
 app.use(bodyPerser.json());
+//body-parser is a middleware (special function in Express) that reads the body of incoming HTTP requests.
+
+// Without it, your server wouldn’t automatically understand JSON data sent from Postman or any client.
+
+// body-parser is like a translator for your server.
+// When Postman sends some JSON data to your server, it arrives as raw text.
+// body-parser translates that raw text into a normal JavaScript object so you can use it directly with req.body.
 
 const Person = require("./models/Person");
 const MenuItem = require("./models/MenuItem");
