f8633b88b3c2aa9a84f1d47413614449e7f40afc
Akash504-ai <santraakash999@gmail.com>
2025-08-09 13:51:51 +0530

    db first commit

---
+++ Hello_World_YT_Channel/Lec_on_db/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const db = require("./db");
+const port = 3000;
+
+const bodyPerser = require("body-parser");
+app.use(bodyPerser.json());
+
+const Person = require("./models/Person");
+const MenuItem = require("./models/MenuItem");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!.....This is my world");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("You are in your profile...");
+});
+
+app.post("/person", async (req, res) => {
+  try {
+    const data = req.body;
+    const newPerson = new Person(data);
+    const response = await newPerson.save();
+    console.log("data saved");
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.get("/person", async (req, res) => {
+  try {
+    const data = await Person.find();
+    console.log("data fatched");
+    res.status(200).json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
