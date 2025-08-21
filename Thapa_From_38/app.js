bceb96ddf72f052a28e660aad37cf4f52fba1d96
Akash504-ai <santraakash999@gmail.com>
2025-08-21 14:48:49 +0530

    .env learning

--- Thapa_From_38/app.js
+++ Thapa_From_38/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/about', (req, res) => {
+  res.send('This is the about page')
+})
+
+// console.log(process);
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Surver is running on port ${port}`)
 })
